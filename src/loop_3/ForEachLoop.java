b576104ee8f8d4852b9913cdddcaa7700e9f77ee
nandah4 <nyustira@gmail.com>
2024-09-05 16:09:09 +0700

    Add For Each Loop

---
+++ src/loop_3/ForEachLoop.java
@@ -0,0 +1,14 @@
+package loop_3;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] myFruits = {
+                "Apple", "Banana", "Manggo", "Orange", "Grapes"
+        };
+
+        // For Each Loop
+        for (String i : myFruits) {
+            System.out.println(i);
+        }
+    }
+}
