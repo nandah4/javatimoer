8cd4ac0f78b296e3bd333e374a3c876d5cf8254c
nandah4 <nyustira@gmail.com>
2024-09-11 22:39:55 +0700

    add folder basic and fill

---
+++ src/basic/loop_3/Do_While.java
@@ -0,0 +1,11 @@
+package basic.loop_3;
+
+public class Do_While {
+    public static void main(String[] args) {
+        int number = 0;
+        do {
+            System.out.println("Ini number " + number);
+            number++;
+        } while (number < 10);
+    }
+}
---
+++ src/basic/third_methods/Methods.java
@@ -0,0 +1,33 @@
+package basic.third_methods;
+
+public class Methods {
+
+    public String myMethod(String name) {
+        return name;
+    }
+
+    public static int myNumber(int a, int b) {
+        return a + b;
+    }
+
+    static void Name(String name) {
+        System.out.println("Nama saya : " + name);
+    }
+
+    static int sum(int a, int b) {
+        int total = a + b;
+        return total;
+
+    }
+
+    public static void main(String[] args) {
+        // Name("Ananda");
+        var c = sum(2, 3);
+        System.out.println(c);
+
+        // Methods obj = new Methods();
+        // System.out.println(obj.myMethod("Nanda\t"));
+        // System.out.println(myNumber(1, 39));
+    }
+
+}
