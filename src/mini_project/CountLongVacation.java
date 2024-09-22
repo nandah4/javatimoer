3f4a92defa9f046e9a68971adcf39e830878bfd1
nandah4 <nyustira6@gmail.com>
2024-09-22 21:05:25 +0700

    feat: init mini project count your vacation

---
+++ src/mini_project/CountLongVacation.java
@@ -0,0 +1,22 @@
+package mini_project;
+
+import java.util.Scanner;
+
+public class CountLongVacation {
+
+    static int countDay(int day) {
+        int a = 10;
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("------------ HITUNG LAMA LIBURAN ANDA ------------\n");
+        System.out.println("Information : ");
+        System.out.println("Sistem ini mengambil 360 hari dalam 1 tahun \n");
+
+        int option;
+
+    }
+}
