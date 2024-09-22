2ed79dead6a92f0c65e7334c02b371f85525670a
nandah4 <nyustira6@gmail.com>
2024-09-22 21:54:18 +0700

    feat: add method hitungTanggalPulang

--- src/mini_project/CountLongVacation.java
+++ src/mini_project/CountLongVacation.java
@@ -3,20 +3,52 @@ package mini_project;
 import java.util.Scanner;
 
 public class CountLongVacation {
+    static Scanner input = new Scanner(System.in);
+    static int option;
+    static String optionInner = "";
 
-    static int countDay(int day) {
-        int a = 10;
-        return a;
+    static void hitungTanggalPulang() {
+        // .equals untuk membandingkan apakah kedua variabel memiliki nilai yang sama
+        do {
+            if (!optionInner.equals("x") || optionInner.equals("a")) {
+                System.out.print("Masukkan tanggal berangkat anda : ");
+                int tanggalBerangkat = input.nextInt();
+                System.out.print("Masukkan bulan berangkat anda : ");
+                int bulanBerangkat = input.nextInt();
+                System.out.print("Masukkan tahun berangkat anda : ");
+                int tahunBerangkat = input.nextInt();
+
+                System.out.println(
+                        "Hasil anda : " + tahunBerangkat + " " + bulanBerangkat + " " + tanggalBerangkat + "\n");
+
+                System.out.print("Close (x) or Continue (a) : ");
+                optionInner = input.next();
+                System.out.println("");
+            } else {
+                System.out.println("Keluar dari Hitung tanggal pulang");
+            }
+
+        } while (!optionInner.equals("x") && optionInner.equals("a"));
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
         System.out.println("------------ HITUNG LAMA LIBURAN ANDA ------------\n");
         System.out.println("Information : ");
         System.out.println("Sistem ini mengambil 360 hari dalam 1 tahun \n");
 
-        int option;
+        do {
+            System.out.println("1. Hitung tanggal pulang");
+            System.out.println("2. Hitung lama hari");
+            System.out.print("Masukkan Pilihan Anda : ");
+            option = input.nextInt();
+            if (option == 1) {
+                hitungTanggalPulang();
+                optionInner = ""; // reset optionInner
+            } else {
+                break;
+            }
+        } while (option != 2);
 
     }
 }
