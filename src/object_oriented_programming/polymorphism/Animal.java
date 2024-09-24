d29a68e93ccf9735f50ce7e0b2151f8f37468ddc
nandah4 <nyustira6@gmail.com>
2024-09-24 16:39:24 +0700

    feat: add polymorpism section

---
+++ src/object_oriented_programming/polymorphism/Animal.java
@@ -0,0 +1,26 @@
+package object_oriented_programming.polymorphism;
+
+class Animal {
+    String animalType;
+
+    Animal(String animalType) {
+        this.animalType = animalType;
+    }
+
+    void voice(String voice) {
+        System.out.println(this.animalType + " suaranya " + voice);
+    }
+}
+
+class Kucing extends Animal {
+    Kucing(String animalType) {
+        super(animalType);
+    }
+}
+
+class Kambing extends Kucing {
+    Kambing(String animalType) {
+        super(animalType);
+    }
+
+}
