41c921249790c0b0dffc7c5c1672a0c610cab8b5
nandah4 <nyustira6@gmail.com>
2024-09-24 15:42:51 +0700

    feat: add super keyword

--- src/object_oriented_programming/inheritances/Kendaraan.java
+++ src/object_oriented_programming/inheritances/Kendaraan.java
@@ -7,13 +7,34 @@ class Kendaraan {
     String jenis;
     String warna;
 
+    Kendaraan(String jenis, String warna) {
+        /*
+         * Constructor ini memiliki pararms, maka salah satu dari properties wajib
+         * diakses dengan method super
+         */
+        this.jenis = jenis;
+        this.warna = warna;
+    }
+
     void bergerak(String name) {
         System.out.println(this.jenis + " " + name + " dapat bergerak");
+        System.out.println(this.jenis + " " + warna + " mengkilau");
     }
+
+    // Contoh untuk penerapan super
+    // int myNumber() {
+    // return 10;
+    // }
+
 }
 
 class Mobil extends Kendaraan {
     int jml_pintu;
+
+    Mobil(String jenis, String warna, int jml_pintu) {
+        super(jenis, null); // contoh
+        this.jml_pintu = jml_pintu;
+    }
 
     @Override
     /*
@@ -23,6 +44,19 @@ class Mobil extends Kendaraan {
      */
     void bergerak(String name) {
         // Hanya bisa memodifikasi return atau sout nya
-        System.out.println(name + " berbelok ke kiri");
+        System.out.println(name + " berwarna " + this.warna + " dapat berbelok ke kiri");
+        System.out.println(name + " memiliki " + this.jml_pintu + " pintu");
     }
+
+    void getMethodParent(String name) {
+        super.bergerak(name);
+        // Keyword keyword untuk mengakses parent method yang di override
+    }
+
+    // contoh penerapan keyword super bisa
+    // void getMyNumber() {
+    // System.out.println(super.myNumber());
+    // ;
+    // }
+
 }
--- src/object_oriented_programming/inheritances/KendaraanMain.java
+++ src/object_oriented_programming/inheritances/KendaraanMain.java
@@ -3,20 +3,24 @@ package object_oriented_programming.inheritances;
 public class KendaraanMain {
     public static void main(String[] args) {
         // Instance dari parent class
-        Kendaraan mobil1 = new Kendaraan();
-        mobil1.jenis = "Pesawat";
-        mobil1.warna = "Putih";
-        mobil1.bergerak("Garuda Indonesia");
+        // mobil1.jenis = "Pesawat";
+        // mobil1.warna = "Putih";
+        // mobil1.bergerak("Garuda Indonesia");
+        // System.out.println(mobil1.myNumber());
+        Kendaraan mobil1 = new Kendaraan("Kapal Laut", "Blue");
+        mobil1.bergerak("Harmony");
 
         System.out.println("");
 
         // Instance dari subclass
-        Mobil mobil2 = new Mobil();
-        mobil2.jenis = "Mobil";
-        mobil2.warna = "Yellow";
-        mobil2.jml_pintu = 2;
-        mobil2.bergerak("Mc Larren 90s");
-        // System.out.println(mobil2.jenis + " " + mobil2.warna + " dengan jumlah pintu
-        // " + mobil2.jml_pintu);
+        // mobil2.jenis = "Mobil";
+        // mobil2.warna = "Yellow";
+        // mobil2.jml_pintu = 2;
+        // mobil2.bergerak("Mc Larren 90s");
+        // mobil2.getMethodParent("Lamborghini");
+        // mobil2.getMyNumber();
+        Mobil mobil2 = new Mobil("Mobil", "Red", 2);
+        mobil2.bergerak("Toyota");
+
     }
 }
