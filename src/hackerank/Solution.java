c6e6518c447d56222a1b2d32503e6879eac11a4c
nandah4 <nyustira6@gmail.com>
2024-10-02 15:40:14 +0700

    feat: add solution for formatted print

---
+++ src/hackerank/Solution.java
@@ -0,0 +1,40 @@
+package hackerank;
+
+import java.util.Scanner;
+
+public class Solution {
+    /*
+     * printf dalam Java adalah untuk mencetak teks ke output dengan format yang
+     * ditentukan. Ini memungkinkan kontrol lebih rinci terhadap bagaimana data,
+     * seperti string, angka, atau objek, ditampilkan. Dengan printf, kamu dapat
+     * mengatur tata letak (alignment), lebar tampilan, jumlah desimal, dan
+     * menambahkan elemen seperti padding dengan karakter tertentu.
+     * 
+     * % diikuti oleh karakter format yang menunjukkan bagaimana objek berikutnya
+     * akan diformat.
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+
+            // Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+
+        System.out.println("================================");
+        FormattedPrint();
+    }
+
+    static void FormattedPrint() {
+        int number = 10;
+        double doubleNum = 10.3;
+        String name = "John Doe";
+
+        System.out.printf("number : %d%n", number);
+        System.out.printf("double : %f%n", doubleNum);
+
+    }
+}
